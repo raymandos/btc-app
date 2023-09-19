4431c63a8e35e740fd71d9fc703c7c3d95c3483f
Raymando <alungu@hotmail.com>
2023-09-19 21:50:58 +0300

    Added banner and API configuration

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Container, Typography } from '@material-ui/core';
+import React from 'react';
+import './Banner.css';
 
 const Banner = () => {
   return (
-    <div className="banner">
-
+    <div className='banner'>
+        <Container className='bannerContent'>
+        <div className='tagline'>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            BTC Romania Pro
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Everything you need to know about cryptocurrency
+          </Typography>
+        </div>
+        </Container>
     </div>
   )
 }
