504fffda530b119658d4ef1217afbbf4899b48fe
Raymando <alungu@hotmail.com>
2023-09-19 21:20:10 +0300

    Added basic components and pages

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import { AppBar, Container, MenuItem, Select, ThemeProvider, Toolbar, Typography, createTheme } from '@material-ui/core';
+import React from 'react'
+import './Header.css';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import { CryptoState } from '../CryptoContext';
+
+
+const Header = () => {
+    const history = useHistory();
+    const {currency, setCurrency} = CryptoState();
+    const darkTheme = createTheme({
+        palette: {
+        primary: {
+            main: "#fff",
+        },
+        type: 'dark',
+        },
+      });
+  return (
+    <ThemeProvider theme={darkTheme}>
+    <AppBar color='transparent' position='static'>
+        <Container>
+            <Toolbar>
+                <Typography onClick = {() => history.push("/")} className="title">
+                    Bitcoin Romania
+                </Typography>
+                <Select variant = "outlined"
+                style = {{
+                    width: 100,
+                    height: 40,
+                    marginRight: 15,
+                }}
+                value = {currency}
+                onChange = {(e) => setCurrency(e.target.value)}>
+                    <MenuItem value ={'USD'}> USD </MenuItem>
+                    <MenuItem value ={'EUR'}> EUR </MenuItem>
+                </Select>
+            </Toolbar>
+        </Container>
+    </AppBar>
+    </ThemeProvider>
+  )
+}
+
+export default Header;
